94241633aa5c9c6f45f0bc3aaba6447b9dc55db6
PranavPatel2628 <pranavpatel2477@gmail.com>
2025-02-22 14:54:45 +0530

    Layout Page Add and register controller add

---
+++ EmployeManagementSystem/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeManagementSystem.Controllers
+{
+    [Route("/Register")]public class RegisterController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
