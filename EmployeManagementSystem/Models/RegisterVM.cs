b12a42f9d81f6d49951884ed0b5d44eff9dc12ad
chetal patel <89387370+chetalpatel98@users.noreply.github.com>
2025-03-06 21:47:31 +0530

    registor button code

---
+++ EmployeManagementSystem/Models/RegisterVM.cs
@@ -0,0 +1,20 @@
+﻿namespace EmployeManagementSystem.Models
+{
+    public class RegisterVM
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Salary { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string JoinDate { get; set; }
+        public int PreviousExperince { get; set; }
+        public string Deparment { get; set; }
+        public IFormFile profilePhoto { get; set; }
+        public string profilePhotoUrl { get; set; }
+    }
+}
